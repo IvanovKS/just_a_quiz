3e86b112f374d86cdf21e315b8108f71b17c8c75
IvanovKS <Skitchi@mail.ru>
2025-02-07 23:52:29 +0300

    Add routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
+import Rules from './pages/Rules';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="app">
-      <Home />
-      <Quiz />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="rules" element={<Rules />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
