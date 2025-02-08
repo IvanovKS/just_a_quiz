599f1993bc9ee517f3104cd2c96c45a93c8ac78c
IvanovKS <Skitchi@mail.ru>
2025-02-08 23:55:09 +0300

    Add header, first simple styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ThemeSwitcher from './ThemeSwitcher';
+import styles from './Header.module.css';
+import avatar from '../assets/images/android-chrome-192x192.png';
 
 function Header() {
   return (
-    <nav>
-      <NavLink to="/" end>
-        Go home
-      </NavLink>
-      <NavLink to="/rules">Rules</NavLink>
-      <NavLink to="/quiz">Quiz</NavLink>
-    </nav>
+    <header className={styles.header}>
+      <div className={styles.headerLeft}>
+        <img src={avatar} alt="avatar" />
+        <h1>Just a quiz</h1>
+      </div>
+      <nav className={styles.headerCenter}>
+        <NavLink to="/" end>
+          Go home
+        </NavLink>
+        <NavLink to="/rules">Rules</NavLink>
+        <NavLink to="/quiz">Quiz</NavLink>
+      </nav>
+      <div className={styles.headerRight}>
+        <ThemeSwitcher />
+      </div>
+    </header>
   );
 }
 
---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ThemeSwitcher() {
+  return (
+    <div>
+      <button>new theme</button>
+    </div>
+  );
+}
+
+export default ThemeSwitcher;
