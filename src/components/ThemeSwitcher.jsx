1fc1af294424ef0cfead12becc72a398106cb6a5
IvanovKS <Skitchi@mail.ru>
2025-02-09 00:41:36 +0300

    Add icons for switcher

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaSun } from 'react-icons/fa';
+import { MdOutlineDarkMode } from 'react-icons/md';
+import styles from './ThemeSwitcher.module.css';
 
 function ThemeSwitcher() {
+  const [switcher, setSwitcher] = useState(true);
   return (
     <div>
-      <button>new theme</button>
+      <button
+        className={styles.switcher}
+        onClick={() => setSwitcher(!switcher)}
+      >
+        {switcher === true ? <FaSun /> : <MdOutlineDarkMode />}
+      </button>
     </div>
   );
 }
