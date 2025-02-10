fad0e4a18f646e43e456bc3c3edbb9720dbfd0b0
IvanovKS <Skitchi@mail.ru>
2025-02-10 21:02:36 +0300

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import styles from './Footer.module.css';
+import clickAnimation from '../assets/animations/clickAnimation.json';
 
 function Footer() {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <footer>
-      <Link to="https://github.com/IvanovKS" target="_blank">
-        go to Git
-      </Link>
-      <button>Click</button>
+    <footer className={styles.footer}>
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <Link to="https://github.com/IvanovKS/just_a_quiz" target="_blank">
+          source code on GitHub
+        </Link>
+        {isHovered && (
+          <Lottie
+            animationData={clickAnimation}
+            loop={true}
+            style={{
+              width: 70,
+              height: 70,
+              position: 'absolute',
+              right: '-50px',
+            }}
+          />
+        )}
+      </div>
+      <p>{currentYear}</p>
     </footer>
   );
 }
