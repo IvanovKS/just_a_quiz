59aff8db0b603bc92d3647fe11359493e43bbdf6
IvanovKS <Skitchi@mail.ru>
2025-02-10 21:14:36 +0300

    Change Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import Lottie from 'lottie-react';
 import { Link } from 'react-router-dom';
 import quizAnimation from '../../assets/animations/quizAnimation.json';
+import styles from './Home.module.css';
 
 function Home() {
   return (
-    <div>
-      <Link to="/quiz">
-        <Lottie
-          animationData={quizAnimation}
-          loop={true}
-          style={{ width: 400, height: 400 }}
-        />
-      </Link>
+    <div className={styles.home}>
+      <div></div>
+      <div>
+        <Link to="/quiz">
+          <Lottie
+            animationData={quizAnimation}
+            loop={true}
+            style={{ width: 400, height: 400 }}
+          />
+        </Link>
+      </div>
+      <div></div>
     </div>
   );
 }
