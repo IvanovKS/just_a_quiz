625cb73ecb64882c6c85aafd7e8b8e3baa0ac5dd
IvanovKS <Skitchi@mail.ru>
2025-02-13 14:04:20 +0300

    Add first simple API

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import NotFound from '../NotFound/NotFound';
 
 function Quiz() {
-  return <div>Quiz</div>;
+  const [newQuiz, setNewQuiz] = useState({ results: [] });
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10&category=21&difficulty=easy')
+      .then((res) => res.json())
+      .then((json) => {
+        setNewQuiz(json);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(true);
+      });
+  }, []);
+
+  return (
+    <div>
+      {error ? (
+        <NotFound />
+      ) : (
+        newQuiz.results?.map((elem) => (
+          <p key={elem.correct_answer}>{elem.question}</p>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default Quiz;
